df528932eea2a00c5c2fa33170839feb4326dd5e
Kamrul Saad <skamrul2680@gmail.com>
2023-12-14 01:58:25 +0600

    Initialilzed Frontend App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import PencilTool from "./components/penciltool";
+
+function App() {
+  return (
+    <div className="App">
+      <PencilTool />
+      {/* <DrawingTool /> */}
+    </div>
+  );
+}
+
+export default App;
